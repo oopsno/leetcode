b1ea24ccdea9ba5c0131f9ff4f8280b56ea3728e
Oopsno <yufeiliu94@gmail.com>
2016-02-01 19:16:05 +0800

    added 137 260

--- src/solution/SingleNumber.cpp
+++ src/solution/SingleNumber.cpp
@@ -1,4 +1,8 @@
 #include <vector>
+#include <array>
+#include <bitset>
+#include <numeric>
+#include <functional>
 
 #if (defined(OFFLINE))
 #include "leet/structure.hpp"
@@ -6,13 +10,67 @@ namespace X136 {
 #endif
 
 class Solution {
+  /**
+   * Given an array of integers, every element appears twice except for one. Find that single one.
+   */
  public:
   int singleNumber(std::vector<int> &nums) {
-    int single = 0;
-    for (int num : nums) {
-      single ^= num;
+    return std::accumulate(nums.cbegin(), nums.cend(), 0, std::bit_xor<int>());
+  }
+};
+
+#if (defined(OFFLINE))
+}
+#endif
+
+
+#if (defined(OFFLINE))
+namespace X137 {
+#endif
+
+class Solution {
+  /**
+   * Given an array of integers, every element appears three times except for one. Find that single one.
+   */
+ public:
+  int singleNumber(std::vector<int> &numbers) {
+    constexpr size_t bitwidth = sizeof(int) * 8;
+    int counter[bitwidth] = {};
+    int result = 0;
+    for (int number : numbers) {
+      for (uint32_t i = 0, flag = 1; i < bitwidth; ++i, flag <<= 1) {
+        counter[i] += (flag & number) != 0;
+      }
     }
-    return single;
+    for (uint32_t i = 0, flag = 1; i < bitwidth; ++i, flag <<= 1) {
+      if (counter[i] % 3 != 0) {
+        result |= flag;
+      }
+    }
+    return result;
+  }
+};
+
+#if (defined(OFFLINE))
+}
+#endif
+
+#if (defined(OFFLINE))
+namespace X260 {
+#endif
+
+class Solution {
+ public:
+  std::vector<int> singleNumber(std::vector<int> &nums) {
+    int flag = std::accumulate(nums.begin(), nums.end(), 0, std::bit_xor<int>());
+    flag &= -flag;
+    std::vector<int> res(2, 0);
+    for (auto &a : nums) {
+      if (a & flag) {
+        res[0] ^= a;
+      } else { res[1] ^= a; }
+    }
+    return res;
   }
 };
 
--- test/SingleNumber.cpp
+++ test/SingleNumber.cpp
@@ -5,11 +5,40 @@ namespace X136 {
 
 Solution s;
 
-TEST(SingleNumber, Functionality) {
+TEST(SingleNumber, I) {
   std::vector<int> seq = {1, 2, 3, 4, 3, 2, 1};
   std::vector<int> single = {1};
   EXPECT_EQ(s.singleNumber(seq), 4);
   EXPECT_EQ(s.singleNumber(single), 1);
+}
+
+}
+
+
+namespace X137 {
+
+Solution s;
+
+TEST(SingleNumber, II) {
+  std::vector<int> seq = {1, 1, 1, 2, 2, 4, 2, 3, 3, 3};
+  std::vector<int> single = {1};
+  EXPECT_EQ(s.singleNumber(seq), 4);
+  EXPECT_EQ(s.singleNumber(single), 1);
+}
+
+}
+
+
+namespace X260 {
+
+Solution s;
+
+TEST(SingleNumber, III) {
+  std::vector<int> seq = {1, 1, 2, 4, 2, 3};
+  std::vector<int> answer = {3, 4};
+  std::vector<int> result = s.singleNumber(seq);
+  std::sort(result.begin(), result.end());
+  EXPECT_EQ(result, answer);
 }
 
 }
