2a6caaf95c8078b839b39b2d2dfcce7f5d661dcf
Oopsno <yufeiliu94@gmail.com>
2016-02-24 23:13:39 +0800

    using underlying int[]

--- src/solution/SlidingWindowMaximum.cpp
+++ src/solution/SlidingWindowMaximum.cpp
@@ -9,22 +9,22 @@ namespace X239 {
 
 class Solution {
  public:
-  typedef std::vector<int>::iterator it_t;
-  typedef std::vector<int>::const_iterator cit_t;
+  typedef int *it_t;
+  typedef const int *cit_t;
 
   std::vector<int> maxSlidingWindow(const std::vector<int> &nums, const int k) {
-    if (nums.empty()) {
+    if (k == 0 || nums.empty()) {
       return nums;
     }
     if (k == 1) {
       return nums;
     }
     const size_t size = nums.size() - k + 1;
-    cit_t head = nums.cbegin();
-    cit_t hop = nums.cbegin() + k;
-    const cit_t stop = nums.cbegin() + size;
+    cit_t head = nums.data();
+    cit_t hop = head + k;
+    const cit_t stop = head + size;
     std::vector<int> swmax(size);
-    it_t it = swmax.begin();
+    it_t it = swmax.data();
     while (head != stop) {
       *it++ = *std::max_element(head++, hop++);
     }
