65e7c3e173504d4d9b69608b5ec02ec15b20e452
Yufei Liu <yufeiliu94@gmail.com>
2017-10-10 15:14:37 +0800

    :art: 增强二叉树
    + 添加带有语法糖的构造方式
    + 添加叶节点判断
    + 添加自动内存管理
    + 添加单元测试

--- include/leetcode.hpp
+++ include/leetcode.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <vector>
+#include <optional>
+
 namespace LeetCode {
 
 template<typename T>
@@ -7,6 +10,10 @@ struct TreeNode {
   TreeNode(T value, TreeNode<T> *left, TreeNode<T> *right) : val{value}, left{left}, right{right} {}
 
   TreeNode(T value) : val{value}, left{nullptr}, right{nullptr} {}
+
+  bool is_leaf() const {
+    return left == nullptr and right == nullptr;
+  }
 
   static void release(TreeNode<T> *root) {
     if (root != nullptr) {
@@ -16,8 +23,47 @@ struct TreeNode {
     }
   }
 
+  template<template<typename> typename Container=std::vector>
+  static TreeNode<T> *build(const Container<std::optional<T>> &xs, int x) {
+    if (x > xs.size()) {
+      return nullptr;
+    }
+    const auto val = xs[x - 1];
+    if (not val) {
+      return nullptr;
+    }
+    return new TreeNode<T>(val.value(), build(xs, 2 * x), build(xs, 2 * x + 1));
+  }
+
   TreeNode *left, *right;
   T val;
+};
+
+template<typename T>
+struct Tree {
+  template<template<typename> typename Container=std::vector>
+  Tree(const Container<std::optional<T>> &xs) : node{TreeNode<T>::build(xs, 1)} {}
+
+  template<template<typename> typename Container=std::vector>
+  Tree(const Container<std::optional<T>> &&xs) : node{TreeNode<T>::build(std::move(xs), 1)} {}
+
+  Tree(std::initializer_list<std::optional<T>> il) : node{TreeNode<T>::build(std::vector<std::optional<T>>(il.begin(), il.end()), 1)} {}
+
+  Tree(TreeNode<T> *node) : node{node} {}
+
+  ~Tree() {
+    TreeNode<T>::release(node);
+  }
+
+  inline operator TreeNode<T> *() {
+    return node;
+  }
+
+  inline TreeNode<T> *operator->() {
+    return node;
+  }
+
+  TreeNode<T> *node;
 };
 
 template<typename T>
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,22 @@
 #include <gtest/gtest.h>
 
+#include "leetcode.hpp"
+
+TEST(Tree, Build) {
+  using Tree = LeetCode::Tree<int>;
+  auto x = Tree{1, 2, 3, std::nullopt, 4};
+  // 1st level
+  ASSERT_EQ(x->val, 1);
+  // 2nd level
+  ASSERT_EQ(x->left->val, 2);
+  ASSERT_EQ(x->right->val, 3);
+  ASSERT_TRUE(x->right->is_leaf());
+  // 3rd level
+  ASSERT_EQ(x->left->left, nullptr);
+  ASSERT_EQ(x->left->right->val, 4);
+  ASSERT_TRUE(x->left->right->is_leaf());
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
