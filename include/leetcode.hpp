be2131501964259e06cde529b54e9d7697df9249
Yufei Liu <yufeiilu94@gmail.com>
2021-01-09 14:52:21 +0800

    引入 LeetCode Playground 提供的(反)序列化函数

--- include/leetcode.hpp
+++ include/leetcode.hpp
@@ -1,240 +1,70 @@
 #pragma once
 
-#include <iostream>
-#include <map>
+/**
+ * 基本上都是来自 https://leetcode-cn.com/playground/ 的代码
+ */
+
+#include <string>
 #include <vector>
-#include <optional>
-#include <functional>
-
-#define GTEST_DONT_DEFINE_TEST 1
-#include <gtest/gtest.h>
-
-namespace LeetCode {
-
-template<typename T>
-struct TreeNode {
-  TreeNode(T value, TreeNode<T> *left, TreeNode<T> *right) : val{value}, left{left}, right{right} {}
-
-  explicit TreeNode(T value) : val{value}, left{nullptr}, right{nullptr} {}
-
-  bool is_leaf() const {
-    return left == nullptr and right == nullptr;
-  }
-
-  static void release(TreeNode<T> *root) {
-    if (root != nullptr) {
-      release(root->left);
-      release(root->right);
-      delete root;
-    }
-  }
-
-  template<template<typename> typename Container=std::vector>
-  static TreeNode<T> *build(const Container<std::optional<T>> &xs, int x) {
-    if (x > xs.size()) {
-      return nullptr;
-    }
-    const auto val = xs[x - 1];
-    if (not val) {
-      return nullptr;
-    }
-    return new TreeNode<T>(val.value(), build(xs, 2 * x), build(xs, 2 * x + 1));
-  }
-
-  static bool is_same(TreeNode<T> *lhs, TreeNode<T> *rhs) {
-    if (lhs == rhs) {
-      return true;
-    }
-    if (lhs == nullptr or rhs == nullptr) {
-      return false;
-    }
-    return lhs->val == rhs->val and is_same(lhs->left, rhs->left) and is_same(lhs->right, rhs->right);
-  }
-
-  TreeNode *left, *right;
-  T val;
-};
-
-template<typename T>
-struct Tree {
-  template<template<typename> typename Container=std::vector>
-  explicit Tree(const Container<std::optional<T>> &xs) : node{TreeNode<T>::build(xs, 1)} {}
-
-  template<template<typename> typename Container=std::vector>
-  explicit Tree(Container<std::optional<T>> &&xs) : node{TreeNode<T>::build(std::move(xs), 1)} {}
-
-  Tree(std::initializer_list<std::optional<T>> il) : node{
-      TreeNode<T>::build(std::vector<std::optional<T>>(il.begin(), il.end()), 1)} {}
-
-  explicit Tree(TreeNode<T> *node) : node{node} {}
-
-  ~Tree() {
-    TreeNode<T>::release(node);
-  }
-
-  inline operator TreeNode<T> *() {
-    return node;
-  }
-
-  inline TreeNode<T> *operator->() {
-    return node;
-  }
-
-  bool operator==(const Tree<T> &rhs) const {
-    TreeNode<T>::is_same(node, rhs.node);
-  }
-
-  TreeNode<T> *node;
-};
-
-template<typename T>
+namespace leetcode {
 struct ListNode {
-  explicit ListNode(T x) : val{x}, next{nullptr} {}
-  ListNode(T x,  ListNode<T> *next) : val{x}, next{next} {}
-
-  bool equalTo(const ListNode *rhs) const {
-    auto lhs = this;
-    while (lhs != nullptr and rhs != nullptr) {
-      if (lhs->val != rhs->val) {
-        return false;
-      }
-      lhs = lhs->next;
-      rhs = rhs->next;
-    }
-    return lhs == nullptr and rhs == nullptr;
-  }
-
-  template<template<typename> typename Container=std::vector>
-  static ListNode<T> *build(const Container<T> &xs) {
-    ListNode<T> *head = nullptr, *current = nullptr;
-    for (auto iter = xs.cbegin(); iter != xs.cend(); iter++) {
-      if (head == nullptr) {
-        head = current = new ListNode<T>(*iter);
-      } else {
-        current->next = new ListNode<T>(*iter);
-        current = current->next;
-      }
-    }
-    return head;
-  }
-
-  static size_t size(ListNode<T> *xs) {
-    size_t length = 0;
-    while (xs != nullptr) {
-      xs = xs->next;
-      length += 1;
-    }
-    return length;
-  }
-
-  T val;
+  int val;
   ListNode *next;
+  ListNode(int x) : val(x), next(nullptr) {}
 };
 
-template<typename T>
-struct List {
-  template<template<typename> typename Container=std::vector>
-  explicit List(const Container<T> &xs) : node{ListNode<T>::build(xs)}{}
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
 
-  template<template<typename> typename Container=std::vector>
-  explicit List(const Container<T> &&xs) : node{ListNode<T>::build(std::move(xs))}{}
+int stringToInteger(std::string input);
+std::vector<int> stringToIntegerVector(std::string input);
+ListNode *stringToListNode(std::string input);
+std::string integerVectorToString(std::vector<int> list, int length = -1);
+std::string listNodeToString(ListNode *node);
+std::string boolToString(bool input);
+bool stringToBool(std::string input);
+std::string treeNodeToString(TreeNode *root);
+TreeNode *stringToTreeNode(std::string input);
+std::string stringToString(std::string input);
+void prettyPrintTree(TreeNode *node, std::string prefix = "",
+                     bool isLeft = true);
+void prettyPrintLinkedList(ListNode *node);
 
-  explicit List(std::initializer_list<T> il) : node{ListNode<T>::build(std::vector<T>(il.begin(), il.end()))} {}
+template <typename T> struct deserializer;
 
-  inline operator ListNode<T> *() {
-    return node;
+template <> struct deserializer<std::vector<int>> {
+  std::vector<int> operator()(std::string literal) const {
+    return stringToIntegerVector(literal);
   }
+};
 
-  std::vector<T> to_list() const {
-    ListNode<T> *head = node;
-    std::vector<T> xs;
-    while (head != nullptr) {
-      xs.push_back(head->val);
-      head = head->next;
-    }
-    return xs;
+template <> struct deserializer<ListNode *> {
+  ListNode *operator()(std::string literal) const {
+    return stringToListNode(literal);
   }
-
-  ListNode<T> *node;
 };
 
-}
-
-enum Category {
-  /** 线性结构 */
-  Array, LinkedList, Stack, Heep, Queue, String,
-  /** 查找表 */
-  Map, HashTable,
-  /** 树 */
-  Trees, BinaryTree, BinarySearchTree, Trie,
-  /** 图 */
-  Graph, DFS, BFS, TopRecursion, Pointers, TologicalSort,
-  /** 方法 */
-  DivideAndConquer, DynamicProgramming, Backtracking, ReservoirSampling, Greedy, Memorization,
-  /** 设计 */
-  Design,
-  /** 专项 */
-  BinarySearch, Sort, BitManipulation, Minimax, Implement,
-  /** 数学 */
-  Geometry, Math,
-  /** 其他 */
-  Misc
+template <> struct deserializer<TreeNode *> {
+  TreeNode *operator()(std::string literal) const {
+    return stringToTreeNode(literal);
+  }
 };
 
-namespace LeetCode {
-
-struct SolutionDescriptor {
-  SolutionDescriptor() = default;
-  SolutionDescriptor(const SolutionDescriptor &descriptor);
-  SolutionDescriptor(SolutionDescriptor &&descriptor) noexcept;
-  SolutionDescriptor(size_t id, std::string &&path, std::string &&title, std::vector<Category> &&categories);
-  SolutionDescriptor &operator=(const SolutionDescriptor &descriptor);
-  size_t id = 0;
-  std::string path, title;
-  std::vector<Category> categories;
+template <> struct deserializer<std::string> {
+  std::string operator()(std::string literal) const {
+    return stringToString(literal);
+  }
 };
 
-struct SolutionManager {
-  std::map<size_t, SolutionDescriptor> id_solution;
-  std::map<std::string, SolutionDescriptor> path_solution;
-  std::map<Category, std::vector<SolutionDescriptor>> cat_solutions;
-  void append(const SolutionDescriptor &descriptor);
+template <> struct deserializer<bool> {
+  bool operator()(std::string literal) const { return stringToBool(literal); }
 };
 
-extern SolutionManager solution_manager;
-
-struct SolutionDescriptorInjector {
-  SolutionDescriptorInjector(size_t id,
-                             std::string &&path,
-                             std::string &&title,
-                             std::vector<Category> &&categories);
-};
-
-template<typename Solution, typename Body>
-struct TestBody {
-  explicit TestBody(Body body) : s{}, body{body} {};
-  TestBody (TestBody& tb) : s{tb.s}, body{tb.body} {};
-  Solution s;
-  Body body;
-};
-
-}
-
-using ListNode = LeetCode::ListNode<int>;
-using List = LeetCode::List<int>;
-using TreeNode = LeetCode::TreeNode<int>;
-using Tree = LeetCode::Tree<int>;
-
-#define DESCRIPTION(id, title, ...) \
-  static const LeetCode::SolutionDescriptorInjector __descriptor__(id, __FILE__, title, {__VA_ARGS__})
-
-#define TEST_BODY(test_case, test_name) __test_body_##test_case##test_name##__
-
-#define TEST(test_case, test_name)                                                                          \
-  void TEST_BODY(test_case, test_name)(Solution &);                                                         \
-  GTEST_TEST(test_case, test_name) {                                                                        \
-    LeetCode::TestBody<Solution, std::function<void(Solution&)>> __test__{TEST_BODY(test_case, test_name)}; \
-    __test__.body(__test__.s);                                                                              \
-  }                                                                                                         \
-  void TEST_BODY(test_case, test_name)(Solution &s)
-
+} // namespace leetcode
--- src/leetcode.cpp
+++ src/leetcode.cpp
@@ -1,49 +1,248 @@
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <queue>
+#include <sstream>
+#include <string>
+#include <vector>
+
 #include "leetcode.hpp"
 
-namespace LeetCode {
+namespace leetcode {
 
-SolutionDescriptor::SolutionDescriptor(const SolutionDescriptor &descriptor)
-    : id{descriptor.id}, path{descriptor.path}, title{descriptor.title}, categories{descriptor.categories} {}
+using std::cout;
+using std::endl;
+using std::getline;
+using std::queue;
+using std::string;
+using std::stringstream;
+using std::to_string;
+using std::transform;
+using std::vector;
 
-SolutionDescriptor::SolutionDescriptor(SolutionDescriptor &&descriptor) noexcept
-    : id(descriptor.id),
-      path(std::move(descriptor.path)),
-      title(std::move(descriptor.title)),
-      categories(std::move(descriptor.categories)) {}
+int stringToInteger(string input) { return stoi(input); }
 
-SolutionDescriptor::SolutionDescriptor(const size_t id,
-                                       std::string &&path,
-                                       std::string &&title,
-                                       std::vector<Category> &&categories)
-    : id{id}, path{std::move(path)}, title{std::move(title)}, categories{std::move(categories)} {}
-
-SolutionDescriptor &SolutionDescriptor::operator=(const SolutionDescriptor &descriptor) {
-  id = descriptor.id;
-  path = descriptor.path;
-  title = descriptor.title;
-  categories = descriptor.categories;
+void trimLeftTrailingSpaces(string &input) {
+  input.erase(input.begin(), find_if(input.begin(), input.end(),
+                                     [](int ch) { return !isspace(ch); }));
 }
 
-SolutionDescriptorInjector::SolutionDescriptorInjector(const size_t id,
-                                                       std::string &&path,
-                                                       std::string &&title,
-                                                       std::vector<Category> &&categories) {
-  solution_manager.append(SolutionDescriptor{id, std::move(path), std::move(title),
-                                             std::move(categories)});
+void trimRightTrailingSpaces(string &input) {
+  input.erase(
+      find_if(input.rbegin(), input.rend(), [](int ch) { return !isspace(ch); })
+          .base(),
+      input.end());
 }
 
-void SolutionManager::append(const SolutionDescriptor &descriptor) {
-  id_solution[descriptor.id] = descriptor;
-  path_solution[descriptor.path] = descriptor;
-  for (auto cat : descriptor.categories) {
-    const auto iter = cat_solutions.find(cat);
-    if (iter == cat_solutions.cend()) {
-      cat_solutions.emplace(cat, std::vector<SolutionDescriptor>());
+vector<int> stringToIntegerVector(string input) {
+  vector<int> output;
+  trimLeftTrailingSpaces(input);
+  trimRightTrailingSpaces(input);
+  input = input.substr(1, input.length() - 2);
+  stringstream ss;
+  ss.str(input);
+  string item;
+  char delim = ',';
+  while (getline(ss, item, delim)) {
+    output.push_back(stoi(item));
+  }
+  return output;
+}
+
+ListNode *stringToListNode(string input) {
+  // Generate list from the input
+  vector<int> list = stringToIntegerVector(input);
+
+  // Now convert that list into linked list
+  ListNode *dummyRoot = new ListNode(0);
+  ListNode *ptr = dummyRoot;
+  for (int item : list) {
+    ptr->next = new ListNode(item);
+    ptr = ptr->next;
+  }
+  ptr = dummyRoot->next;
+  delete dummyRoot;
+  return ptr;
+}
+
+string integerVectorToString(vector<int> list, int length) {
+  if (length == -1) {
+    length = list.size();
+  }
+
+  if (length == 0) {
+    return "[]";
+  }
+
+  string result;
+  for (int index = 0; index < length; index++) {
+    int number = list[index];
+    result += to_string(number) + ", ";
+  }
+  return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+string listNodeToString(ListNode *node) {
+  if (node == nullptr) {
+    return "[]";
+  }
+
+  string result;
+  while (node) {
+    result += to_string(node->val) + ", ";
+    node = node->next;
+  }
+  return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+string boolToString(bool input) { return input ? "True" : "False"; }
+
+bool stringToBool(string input) {
+  transform(input.begin(), input.end(), input.begin(), ::tolower);
+  return input == "true";
+}
+
+string treeNodeToString(TreeNode *root) {
+  if (root == nullptr) {
+    return "[]";
+  }
+
+  string output = "";
+  queue<TreeNode *> q;
+  q.push(root);
+  while (!q.empty()) {
+    TreeNode *node = q.front();
+    q.pop();
+
+    if (node == nullptr) {
+      output += "null, ";
+      continue;
     }
-    cat_solutions[cat].push_back(descriptor);
+
+    output += to_string(node->val) + ", ";
+    q.push(node->left);
+    q.push(node->right);
+  }
+  return "[" + output.substr(0, output.length() - 2) + "]";
+}
+
+TreeNode *stringToTreeNode(string input) {
+  trimLeftTrailingSpaces(input);
+  trimRightTrailingSpaces(input);
+  input = input.substr(1, input.length() - 2);
+  if (!input.size()) {
+    return nullptr;
+  }
+
+  string item;
+  stringstream ss;
+  ss.str(input);
+
+  getline(ss, item, ',');
+  TreeNode *root = new TreeNode(stoi(item));
+  queue<TreeNode *> nodeQueue;
+  nodeQueue.push(root);
+
+  while (true) {
+    TreeNode *node = nodeQueue.front();
+    nodeQueue.pop();
+
+    if (!getline(ss, item, ',')) {
+      break;
+    }
+
+    trimLeftTrailingSpaces(item);
+    if (item != "null") {
+      int leftNumber = stoi(item);
+      node->left = new TreeNode(leftNumber);
+      nodeQueue.push(node->left);
+    }
+
+    if (!getline(ss, item, ',')) {
+      break;
+    }
+
+    trimLeftTrailingSpaces(item);
+    if (item != "null") {
+      int rightNumber = stoi(item);
+      node->right = new TreeNode(rightNumber);
+      nodeQueue.push(node->right);
+    }
+  }
+  return root;
+}
+
+string stringToString(string input) {
+  assert(input.length() >= 2);
+  string result;
+  for (int i = 1; i < input.length() - 1; i++) {
+    char currentChar = input[i];
+    if (input[i] == '\\') {
+      char nextChar = input[i + 1];
+      switch (nextChar) {
+      case '\"':
+        result.push_back('\"');
+        break;
+      case '/':
+        result.push_back('/');
+        break;
+      case '\\':
+        result.push_back('\\');
+        break;
+      case 'b':
+        result.push_back('\b');
+        break;
+      case 'f':
+        result.push_back('\f');
+        break;
+      case 'r':
+        result.push_back('\r');
+        break;
+      case 'n':
+        result.push_back('\n');
+        break;
+      case 't':
+        result.push_back('\t');
+        break;
+      default:
+        break;
+      }
+      i++;
+    } else {
+      result.push_back(currentChar);
+    }
+  }
+  return result;
+}
+
+void prettyPrintTree(TreeNode *node, string prefix, bool isLeft) {
+  if (node == nullptr) {
+    cout << "Empty tree";
+    return;
+  }
+
+  if (node->right) {
+    prettyPrintTree(node->right, prefix + (isLeft ? "│   " : "    "), false);
+  }
+
+  cout << prefix + (isLeft ? "└── " : "┌── ") + to_string(node->val) + "\n";
+
+  if (node->left) {
+    prettyPrintTree(node->left, prefix + (isLeft ? "    " : "│   "), true);
   }
 }
 
-SolutionManager solution_manager;
+void prettyPrintLinkedList(ListNode *node) {
+  while (node && node->next) {
+    cout << node->val << "->";
+    node = node->next;
+  }
 
+  if (node) {
+    cout << node->val << endl;
+  } else {
+    cout << "Empty LinkedList" << endl;
+  }
 }
+
+} // namespace leetcode
